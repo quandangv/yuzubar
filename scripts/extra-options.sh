3cec62ca082359403d8bed815382472122209857
quandangv <quandangv@gmail.com>
2021-04-10 09:48:47 +0700

    Add name_desktop as an command in options

--- scripts/extra-options.sh
+++ scripts/extra-options.sh
@@ -1,3 +1,2 @@
 coredump="A:termite --hold -e \"sudo bash -c 'echo core.%e.%p > /proc/sys/kernel/core_pattern'\":}core dump%{A"
-name_desktop="A:./scripts/name-all-desktops.sh:}Name Desktops%{A"
-echo O20 +u $name_desktop -u
+#echo O20 +u $name_desktop -u
