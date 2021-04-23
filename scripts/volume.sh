fe5c7472a8802da0517cf230b29461ffa4c7981c
quandangv <quandangv@gmail.com>
2021-04-23 17:14:58 +0700

    Add module for input volume

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
 char_count=$1
+separator=$2
+control=$3
+label=$4
+
 while :; do
-  raw=$(amixer sget Master)
+  raw=$(amixer sget $control)
   percent=$(sed -n 's/.*\[\([0-9]*%\)\].*/\1/p' <<< $raw)
 
   if [[ "$raw" == *"[off]"* ]]; then
-    echo "%{A:amixer -q sset Master toggle: +u}vol x%{A -u}"
+    echo "%{A:amixer -q sset $control toggle: +u}$label x%{A -u}"
   else
-    filled_count=$(echo $percent $char_count | awk '{ printf "%i", $1 / 100 * $2; }' )
+    filled_count=$(echo $percent | awk "{ sum += \$1 / 100 * $char_count; } END { printf \"%i\", sum/NR; }" )
     empty_count=$(bc <<< "$char_count - $filled_count")
     filled=$(printf '%*s' $filled_count "")
     empty=$(printf '%*s' $empty_count "")
-    echo "%{+u A4:amixer -q sset Master 5%+: A5:amixer -q sset Master 5%-: A:amixer -q sset Master toggle:}vol%{T2 A A:amixer -q sset Master 9%-:} ${filled// /·}%{A A:amixer -q sset Master 9%+:}$2${empty// /·} %{A A4 A5 -u T-}"
+    echo "%{+u A4:amixer -q sset $control 5%+: A5:amixer -q sset $control 5%-: A:amixer -q sset $control toggle:}$label%{T2 A A:amixer -q sset $control 9%-:} ${filled// /·}%{A A:amixer -q sset $control 9%+:}$separator${empty// /·} %{A A4 A5 -u T-}"
   fi
 
   read
